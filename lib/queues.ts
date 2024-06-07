be56e6406b33cd0da11c5ecde175bee5374c88fe
MagicAces <isaac.oniti14@gmail.com>
2024-06-07 16:44:03 +0000

    Disabled the redis feature

--- lib/queues.ts
+++ lib/queues.ts
@@ -1,14 +1,11 @@
 import Queue from "bull";
-import client from "./redis";
-import { checkSessionStatus } from "./sessions";
 
-const redisOptions = {
-  redis: {
-    host: process.env.REDIS_HOST,
-    port: parseInt(process.env.REDIS_PORT || "12345"),
-    password: process.env.REDIS_PASSWORD,
-  },
-};
+// const redisOptions = {
+//   redis: {
+//     host: process.env.REDIS_HOST,
+//     port: parseInt(process.env.REDIS_PORT || "12345"),
+//     password: process.env.REDIS_PASSWORD,
+//   },
+// };
 
-
-export const statusQueue = new Queue("statuses", redisOptions);
+// export const statusQueue = new Queue("statuses", redisOptions);
--- lib/redis.ts
+++ lib/redis.ts
@@ -1,17 +1,17 @@
-import Redis from "ioredis";
+// import Redis from "ioredis";
 
-import { createClient } from "redis";
+// import { createClient } from "redis";
 
-const client = createClient({
-  password: process.env.REDIS_PASSWORD,
-  socket: {
-    host: process.env.REDIS_HOST,
-    port: parseInt(process.env.REDIS_PORT || "12345"),
-  },
-});
+// const client = createClient({
+//   password: process.env.REDIS_PASSWORD,
+//   socket: {
+//     host: process.env.REDIS_HOST,
+//     port: parseInt(process.env.REDIS_PORT || "12345"),
+//   },
+// });
 
-process.on("SIGINT", () => {
-  client.quit().then(() => process.exit(0));
-});
+// process.on("SIGINT", () => {
+//   client.quit().then(() => process.exit(0));
+// });
 
-export default client;
+// export default client;
